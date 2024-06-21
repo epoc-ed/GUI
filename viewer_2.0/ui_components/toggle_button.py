3972cd7cb9ed684888f43524ad06a99792e9d494
KiyofumiTakaba <kiyofumitakaba2018@gmail.com>
2024-06-21 19:22:56 +0200

    refreshing

--- viewer_2.0/ui_components/toggle_button.py
+++
@@ -1,6 +0,0 @@
-from PySide6.QtWidgets import QPushButton
-
-class ToggleButton(QPushButton):
-    def __init__(self, label, window):
-        super().__init__(label, window)
-        self.started = False
