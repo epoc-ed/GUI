02903ad4dc7c2839607a4b194d261773ff601e22
kferjaoui <khalilferjaoui@yahoo.fr>
2024-08-13 17:27:16 +0200

    graceful stop of the rotation/record

--- jungfrau_gui/ui_components/tem_controls/task/control_worker_test.py
+++ jungfrau_gui/ui_components/tem_controls/task/control_worker_test.py
@@ -362,8 +362,8 @@ class ControlWorker(QObject):
                 self.task_thread.quit()
                 self.task_thread.wait() # Wait for the thread to actually finish
 
-                self.task.deleteLater()
-                self.task = None
+                # self.task.deleteLater()
+                # self.task = None
 
         if isinstance(self.task, BeamFitTask):
             self.remove_ellipse.emit() 
@@ -379,7 +379,7 @@ class ControlWorker(QObject):
     def start_record(self):
         print("Start record")
         if self.task.running:
-            self.stop()
+            self.stop_task()
         end_angle = self.tem_action.tem_tasks.update_end_angle.value() # 60
         print(f"End angle + {end_angle}")
         ### filename_suffix = self.tem_action.formatted_filename[:-3]
