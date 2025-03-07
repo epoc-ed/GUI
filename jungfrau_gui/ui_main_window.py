23d4af47daaa6c8023a0d7a23691c8bce04d301d
Khalil <khalilferjaoui@yahoo.fr>
2025-03-07 22:48:44 +0100

    Delete test file

--- jungfrau_gui/ui_main_window.py
+++ jungfrau_gui/ui_main_window.py
@@ -10,7 +10,6 @@ from PySide6.QtWidgets import (QMainWindow, QVBoxLayout, QWidget,
 from PySide6.QtCore import Qt, QObject, QEvent, QTimer
 from PySide6.QtGui import QShortcut, QKeySequence
 from .ui_components.visualization_panel.visualization_panel import VisualizationPanel
-# from .ui_components.visualization_panel.visualization_panel_test import VisualizationPanel
 from .ui_components.tem_controls.tem_controls import TemControls
 from .ui_components.file_operations.file_operations import FileOperations
 from .ui_components.utils import create_gaussian
